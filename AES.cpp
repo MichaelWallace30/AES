899bd04412ae07b22e3bda9f3686f0369c1978a2
MichaelWallace30 <Michael.Wallace30@gmail.com>
2017-01-27 11:48:06 -0800

    Class structure

---
+++ AES.cpp
@@ -0,0 +1,11 @@
+#include "AES.h"
+
+
+using namespace AES_NAMESPACE;
+
+
+uint32_t AES::cypher(uint8_t *buffer, uint32_t size, uint8_t* key
+{
+    //check if block size if %16
+    //either pad or throw error
+}
---
+++ AES.h
@@ -0,0 +1,37 @@
+#include <stdint.h>
+
+
+namespace AES_NAMESPACE{
+
+#define BLOCK_SIZE 16
+#define MATRIX_SIZE 4 //sqrt(BLOCK_SIZE)
+
+    class AES{
+
+        AES();
+        ~AES();
+
+        uint32_t cypher(uint8_t *buffer, uint32_t size, uint8_t* key);
+        
+
+    private:
+
+        uint8_t block[MATRIX_SIZE][MATRIX_SIZE];
+
+        /** Sub Bytes **/
+
+        /** Shift Rows **/
+
+        /** Mix Columns 
+            Galois Field matrix multiplication **/
+
+        /** Add Round Key **/
+
+        
+
+
+    };
+
+
+};
+
