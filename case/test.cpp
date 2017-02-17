bdd8a5cb751622703ba68125639c6a011d9ad308
CheezBoiger <alexander.garcia18@yahoo.com>
2017-02-17 02:00:59 -0800

    Cleaned up code, added case

---
+++ case/test.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "../AES.h"
+
+
+#define debug(stmt, ...) std::printf(stmt, ##__VA_ARGS__)
+
+
+int main(int c, char *args[]) {
+  AES_NAMESPACE::AES a;
+  std::cin.ignore();
+  return 0;
+}
